e668ad63f66370d4df8cf313766a4ae07134e0ae
Arun Suresh <akuma14@searshc.com>
2015-01-09 15:14:41 -0600

    sample webpages

--- service.py
+++ service.py
@@ -8,6 +8,26 @@ def index():
     return render_template('index.html')
 
 
+@service.route('/xxx')
+def test():
+    return render_template('index.html')
+
+
+@service.route('/landing')
+def landing():
+    return render_template('landing.html')
+
+
+@service.route('/landing1')
+def landing1():
+    return render_template('landing1.html')
+
+
+@service.route('/landing2')
+def landing2():
+    return render_template('landing2.html')
+
+
 @service.route('/deeds/userid/<userid>')
 def get_deeds(userid):
     lst = [
@@ -15,6 +35,14 @@ def get_deeds(userid):
         {'userid': userid, 'deed': {'a': 33, 'b': 111}},
         ]
     return jsonify(result=lst)
+
+
+@service.route('/info/user/<userid>/<roomid>')
+def set_info(userid, roomid):
+    info = "Server received information for user "+userid+" for room " + roomid \
+        + "."
+    return jsonify(result=info)
+
 
 if __name__ == '__main__':
     service.run(
